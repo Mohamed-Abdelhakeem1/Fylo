59a8d9d46cdff19f135af06de15ffbd02a96689a
Mohamed-Abdelhakeem1 <mohamedabdelhakeem197@gmail.com>
2025-04-08 22:33:57 +0200

    edit in url href

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
   const links = ["Features", "Team", "Signin"];
   const linksList = links.map((el, idx) => (
     <li
-      className="h-fit opacity-90 hover:opacity-100 hover:underline  transition-all duration-500"
+      className="h-fit cursor-pointer opacity-90 hover:opacity-100 hover:underline  transition-all duration-500"
       key={idx}
     >
-      <a className="block p-1" href={"/"}>
+      <p className="block p-1">
         {el}
-      </a>
+      </p>
     </li>
   ));
   const head = useRef();
@@ -32,9 +32,9 @@ const Header = () => {
       className="py-7 sticky top-0 transition-all duration-100 bg-[#1c2433] z-50"
     >
       <div className="container text-white flex flex-col sm:flex-row justify-between items-center gap-3 sm:gap-0">
-        <a href="/">
+        <p>
           <img className="w-40" src={logo} alt="Logo Image" />
-        </a>
+        </p>
         <nav className="flex align-items-center">
           <ul className="flex justify-between gap-5 lg:gap-10 xl:gap-16 items-center">
             {linksList}
